8be16620e55a678aabdbd6b7d3841f20f62930a3
Vahid Mousavi <vahid.mousavii93@gmail.com>
2025-07-31 21:45:04 +0330

    Implement UserController, UserRepository, UserService

---
+++ src/main/java/io/devotel/user/service/UserService.java
@@ -0,0 +1,16 @@
+package io.devotel.user.service;
+
+import io.devotel.common.GeneralResponseDto;
+import io.devotel.user.dto.AddUserDTO;
+import io.devotel.user.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+
+    GeneralResponseDto<UserDTO> addUser(AddUserDTO addUserDTO);
+
+    GeneralResponseDto<UserDTO> getUserById(Long id);
+
+    GeneralResponseDto<List<UserDTO>> getAllUsersResponse();
+}
