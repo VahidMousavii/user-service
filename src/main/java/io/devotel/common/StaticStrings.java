61e2e277a8ea50044e49874f4295541c14a094ff
Vahid Mousavi <vahid.mousavii93@gmail.com>
2025-07-31 21:36:38 +0330

    Add StaticStrings for storing constant strings

---
+++ src/main/java/io/devotel/common/StaticStrings.java
@@ -0,0 +1,12 @@
+package io.devotel.common;
+
+/**
+ * Central place for all static string constants used in responses, logs, etc.
+ */
+public final class StaticStrings {
+
+    private StaticStrings() {}
+
+    public static final String SUCCESS_MESSAGE = "Successful";
+    public static final String USER_NOT_FOUND = "User not found";
+}
